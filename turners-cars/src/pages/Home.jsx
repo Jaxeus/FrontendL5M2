a629170f47e3d0f16cd0b2487fec1f1f77f3f92c
Jason Cluley <jaxeus@gmail.com>
2023-12-04 22:19:59 +1300

    Installed Axios. Imported all data from Azure component from other repo into Home component. Seems to be working, haven't tested API

--- turners-cars/src/pages/Home.jsx
+++ turners-cars/src/pages/Home.jsx
@@ -4,13 +4,126 @@ import styles from "./Home.module.css";
 import MainMenu from "./MainMenu";
 import TopMenu from "./TopMenu";
 import Header from "./Header";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [imageUrl, setImageUrl] = useState("");
+  const [result, setResult] = useState(null);
+  const [carImage, setCarImage] = useState(null);
+
+  const analyzeImage = async () => {
+    const apiKey = process.env.REACT_APP_AZURE_API_KEY;
+    const endpoint = process.env.REACT_APP_AZURE_ENDPOINT;
+    // const params = {
+    //   visualFeatures: "Categories,Description,Color",
+    //   details: "",
+    //   language: "en",
+    // };
+    // /vision/v3.1/analyze (cut)
+    try {
+      console.log(imageUrl);
+      const response = await axios.post(
+        `${endpoint}`,
+        { url: imageUrl },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": apiKey,
+          },
+          // params,
+        }
+      );
+
+      //-----------------FUNCTIONAL CODE-----------------//
+      // Andrew helped with the below
+      const tagResults = response.data.tagsResult;
+      const carFilter = tagResults
+        ? tagResults.values.filter((item) => item.name === "car")
+        : [];
+
+      console.log(response.data.tagResults);
+      if (carFilter.length > 0) {
+        //Andrew helped with the above
+        console.log("This is a car");
+        setResult(
+          `This is indeed a ${
+            response.data.tagsResult.values.filter(
+              (item) => item.name === "car"
+            )[0].name
+          }`
+        );
+        setCarImage(
+          <img width="300px" height="auto" alt="car" src={imageUrl} />
+        );
+      } else {
+        console.log(
+          "The Azure API hasn't recognised that this image contains a car ⛔🚗"
+        );
+        setResult(
+          `This image doesn't appear to contain a 🚗 - Either the Azure Computer Vision has failed, or you should have gone to Specsavers`
+        );
+        setCarImage(
+          <img width="300px" height="auto" alt="car" src={imageUrl} />
+        );
+      }
+
+      //-------------SET RESULT ONLY---------------//
+      // setResult(
+      //   response.data.tagsResult.values.filter((item) => item.name === "car")[0]
+      //     .name
+      // );
+
+      //--------------TRYING STUFF-------------//
+      // console.log(
+      //   response.data.tagsResult.values.filter((item) => item.name === "car")[0].name
+      // );
+      // if (response.data.tagsResult.values[3].name === "car") {
+      // console.log("This is a car");
+      // setResult(
+      //   `This is indeed a ${response.data.tagsResult.values[3].name}`
+      // );
+      // setCarImage(
+      //   <img width="200px" height="auto" alt="car" src={imageUrl} />
+      // );}
+      // else {
+      //   console.log("Not today, my friend");
+      // }
+      // console.log(response.data.tagsResult.values["name"]);
+      // setResult(
+      //   response.data.tagsResult.values.filter((item) => item.name === "car")[0]
+      //     .name
+      // );
+    } catch (error) {
+      console.error("Error analyzing image:", error);
+    }
+  };
+
   return (
     <div>
-      <div>This is the Home page</div>
-      <Link to="/turners-subscription">Turners Subscription</Link>
-      <Link to="/trucks-and-machinery">Trucks and Machinery</Link>
+      <TopMenu />
+      <Header />
+      <MainMenu />
+      <div className={styles.azureContainer}>
+        <div className={styles.formContainer}>
+          <h2>Analyse your car!</h2>
+          <input
+            type="text"
+            placeholder="Enter image URL"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+          <button onClick={analyzeImage}>Analyse Image</button>
+        </div>
+
+        {result && (
+          <div>
+            <h3 className={styles.resultHeading}>Analysis Result:</h3>
+            <pre>{JSON.stringify(result, null, 2)}</pre>
+          </div>
+        )}
+        <div>{carImage}</div>
+      </div>
     </div>
   );
 }
