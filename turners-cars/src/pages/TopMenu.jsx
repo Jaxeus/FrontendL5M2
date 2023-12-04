07a7e5b9cc31dad60e815af82a096648ca614b27
Jason Cluley <jaxeus@gmail.com>
2023-12-04 22:47:12 +1300

    Added ENV file? Added two links to Subscription and Trucks components, and fixed the CSS for links.

--- turners-cars/src/pages/TopMenu.jsx
+++ turners-cars/src/pages/TopMenu.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./TopMenu.module.css";
+import { Link } from "react-router-dom";
+// import Trucks from "./Trucks";
 
 export default function TopMenu() {
   return (
@@ -7,8 +9,16 @@ export default function TopMenu() {
       <button className={`${styles.menuButton} ${styles.firstButton}`}>
         Cars
       </button>
-      <button className={styles.menuButton}>Turners Subscription</button>
-      <button className={styles.menuButton}>Trucks and Machinery</button>
+      <button className={styles.menuButton}>
+        <Link to="/turners-subscription" className={styles.link}>
+          Turners Subscription
+        </Link>
+      </button>
+      <button className={styles.menuButton}>
+        <Link to="/trucks-and-machinery" className={styles.link}>
+          Trucks and Machinery
+        </Link>
+      </button>
       <button className={styles.menuButton}>Damaged & End of Life</button>
       <button className={styles.menuButton}>Motorcycles</button>
       <button className={styles.menuButton}>General Goods</button>
