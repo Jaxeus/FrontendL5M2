907694b8bdde790e29a2ad1876f486f93f72c89e
Jason Cluley <jaxeus@gmail.com>
2023-12-05 20:02:29 +1300

    Added some axios and fetch work to the Car Analysis component.

--- turners-cars/src/pages/CarAnalysis.jsx
+++ turners-cars/src/pages/CarAnalysis.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./CarAnalysis.module.css";
-// import axios from "axios";
+import axios from "axios";
 
 export default function CarAnalysis() {
   //   POST https://eastus.api.cognitive.microsoft.com/computervision/imageanalysis:analyze?api-version=2023-04-01-preview&features=tags HTTP/1.1
@@ -12,11 +12,48 @@ export default function CarAnalysis() {
   //   "url": "https://www.seat.co.nz/content/dam/public/seat-website/models/tarraco/exterior-colours/tarracocclite-blueatlantic.png"
   // }
 
+  const apiEndpoint = process.env.REACT_APP_VISION_ENDPOINT;
+  const apiKey = process.env.REACT_APP_VISION_KEY;
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Ocp-Apim-Subscription-Key": apiKey,
+  };
+
+  const postData = {
+    url: "https://www.seat.co.nz/content/dam/public/seat-website/models/tarraco/exterior-colours/tarracocclite-blueatlantic.png",
+  };
+
+  function postie() {
+    axios
+      .post(apiEndpoint, postData, { headers: headers })
+      .then((response) => {
+        console.log("Response:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  // fetch(apiEndpoint, {
+  //   method: "POST",
+  //   headers,
+  //   body: JSON.stringify(postData),
+  // })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log("Response: ", data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error: ", error);
+  //   });
+
   return (
     <div className={styles.carAnalysisContainer}>
       <div>
         <h1>Car Analysis</h1>
         <p>Car Analysis Page</p>
+        <button onClick={postie}>Postie</button>
       </div>
     </div>
   );
