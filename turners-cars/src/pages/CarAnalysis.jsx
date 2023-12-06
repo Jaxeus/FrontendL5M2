4be384dacac9e051958b4bedc40c959687a9e1fa
Jason Cluley <jaxeus@gmail.com>
2023-12-06 21:27:29 +1300

    Frontend and Backend are working together now. Need to do things with the data now.

--- turners-cars/src/pages/CarAnalysis.jsx
+++ turners-cars/src/pages/CarAnalysis.jsx
@@ -1,59 +1,42 @@
 import React from "react";
 import styles from "./CarAnalysis.module.css";
 import axios from "axios";
+import { useState } from "react";
 
 export default function CarAnalysis() {
-  //   POST https://eastus.api.cognitive.microsoft.com/computervision/imageanalysis:analyze?api-version=2023-04-01-preview&features=tags HTTP/1.1
-  // Host: eastus.api.cognitive.microsoft.com
-  // Content-Type: application/json
-  // Ocp-Apim-Subscription-Key:
+  const [imageURL, setImageURL] = useState("");
 
-  // {
-  //   "url": "https://www.seat.co.nz/content/dam/public/seat-website/models/tarraco/exterior-colours/tarracocclite-blueatlantic.png"
-  // }
+  async function analyseImage(e) {
+    e.preventDefault();
+    console.log(typeof imageURL);
 
-  const apiEndpoint = process.env.REACT_APP_VISION_ENDPOINT;
-  const apiKey = process.env.REACT_APP_VISION_KEY;
-
-  const headers = {
-    "Content-Type": "application/json",
-    "Ocp-Apim-Subscription-Key": apiKey,
-  };
-
-  const postData = {
-    url: "https://www.seat.co.nz/content/dam/public/seat-website/models/tarraco/exterior-colours/tarracocclite-blueatlantic.png",
-  };
-
-  function postie() {
-    axios
-      .post(apiEndpoint, postData, { headers: headers })
-      .then((response) => {
-        console.log("Response:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
+    try {
+      const response = await axios.post("http://localhost:4000/azure", {
+        imageURL,
       });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error, "Server failed");
+    }
   }
-
-  // fetch(apiEndpoint, {
-  //   method: "POST",
-  //   headers,
-  //   body: JSON.stringify(postData),
-  // })
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log("Response: ", data);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error: ", error);
-  //   });
 
   return (
     <div className={styles.carAnalysisContainer}>
       <div>
         <h1>Car Analysis</h1>
         <p>Car Analysis Page</p>
-        <button onClick={postie}>Postie</button>
+      </div>
+      <div>
+        <form id="form">
+          <h2>Analyse your car 🐣</h2>
+          <input
+            type="text"
+            placeholder="Enter image URL"
+            value={imageURL}
+            onChange={(e) => setImageURL(e.target.value)}
+          />
+          <button onClick={(e) => analyseImage(e)}>Analyse Image</button>
+        </form>
       </div>
     </div>
   );
